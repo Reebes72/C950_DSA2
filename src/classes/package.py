d63e91271c42c888cdcae2544960b0f9da91781d
Andrew Reeves <Andrew.Reeves@bcbsal.org>
2025-08-06 14:51:02 -0500

    Working on package.py

--- src/classes/package.py
+++ src/classes/package.py
@@ -1,3 +1,28 @@
 class Package:
-    def __init__(self):
-        pass
+    def __init__(self, address, deadline, city, zip, weight, status, delivery, load, id):
+        self.delivery_address = address
+        self.deadline = deadline
+        self.city = city
+        self.zip_code = zip
+        self.package_weight = weight
+        self.package_id = id
+        self.delivery_time = delivery
+        self.loading_time = load
+
+    def __str__(self):
+        nl = "\n"
+        return f"""Package: {self.id}{nl}
+    Address: {self.address}, {self.city},
+    {self.zip_code}{nl}Weight:
+    {self.package_weight}{nl}
+    Delivery Time: {self.delivery_time}{nl}
+    Loading Time: {self.loading_time}"""
+
+    def __repr__(self):
+        nl = "\n"
+        return f"""Package: {self.id}{nl}
+    Address: {self.address}, {self.city},
+    {self.zip_code}{nl}Weight:
+    {self.package_weight}{nl}
+    Delivery Time: {self.delivery_time}{nl}
+    Loading Time: {self.loading_time}"""
