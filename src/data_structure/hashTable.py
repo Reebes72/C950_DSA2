1b7d6667279e4b4984261a5546328f58d97ebe0a
Andrew Reeves <Andrew.Reeves@bcbsal.org>
2025-08-07 19:24:34 -0500

    hashMap work

--- src/data_structure/hashTable.py
+++ src/data_structure/hashTable.py
@@ -1,3 +1,17 @@
 class hashTable:
-    def __init__(self):
-        
+
+    def __init__(self, capacity: int = 10):
+        self.hashMap = []
+        for i in range(capacity):
+            self.hashMap.append([])
+
+    def hashInsert(self, package_id, value):
+        self.values[package_id % 10] = value
+
+    def hashSearch(self, package_id):
+        key = package_id % 10
+        return self.values[key]
+
+    def __len__(self):
+        return len(self.values)
+            
--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import csv
-import classes.package as Package
+from classes.package import Package
+FILEPATH: str = "src/resources/csv/package_file.csv"
 
-FILEPATH: str = f"src/resources/csv/package_file.csv"
 
 def initialize_packageFile(filename) -> list:
     packages: list = []
     with open(filename, 'r') as f:
         reader = csv.reader(f)
         for line in reader:
-            item = Package.Package(list(line))
+            item = Package(list(line))
             print(item)
             packages.append(item)          
     return packages
