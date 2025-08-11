a335f7ebfadc1b4fc6976864336ced9129cb98b0
Andrew Reeves <Andrew.Reeves@bcbsal.org>
2025-08-10 22:55:16 -0500

    Driver class built out.

--- src/classes/driver.py
+++ src/classes/driver.py
@@ -1,4 +1,20 @@
 class Driver:
+    # Initialization method: Sets driver ID.
     def __init__(self, driver_id):
         self.driver_id = driver_id
+        self.truck = None
+
+    #Gets a truck for the driver, checks if any trucks are available
+    # and assigns driver to truck.
+    def getTruck(self, trucks):
+        for truck in trucks:
+            if truck.driver is None:
+                truck.driver = self
+                self.truck = truck
+                return True
+        return False
+    
+    #Remove Driver from truck and vice versa.
+    def dropTruck(self):
+        self.truck.driver = None
         self.truck = None
