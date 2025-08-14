a3d9c30d0fde5bf65ff1db4a67724ae696bd462d
Andrew Reeves <Andrew.Reeves@bcbsal.org>
2025-08-14 14:07:33 -0500

    CLI Work

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 from data_structure.hashTable import hashTable
 from classes.truck import Truck
+from classes.package import Package
+from classes.driver import Driver
 
 def main_menu(table: hashTable, trucks: list):
     print("!!!!!!!!!!!!!!!!!!!!")
@@ -45,7 +47,6 @@ def general(table: hashTable, trucks: list):
 def print_mileage(trucks: list, report_time: datetime):
     report_timedelta = timedelta(hours=report_time.hour, minutes=report_time.minute)
     total_mileage = 0
-    truck: Truck
     for truck in trucks:
         if len(truck.mileage_times) > 0:
             index = len(truck.mileage_times) - 1
@@ -63,5 +64,58 @@ def print_mileage(trucks: list, report_time: datetime):
     print("\nThe total mileage of all trucks at " + report_time.strftime("%I:%M %p") + " is %0.2f miles" % total_mileage)
 
 
-def query_package():
-    pass
+def query_package(table: hashTable, trucks: list):
+    report_time: datetime = prompt_time()
+    id = prompt_package_id(table)
+    print("!!!!!!!!!!!!!!!!!!!!")
+    print("Querying: " + report_time.strftime("%I:%M %p"))
+    print("!!!!!!!!!!!!!!!!!!!!")
+    display_query(table, id, report_time)
+    main_menu(table, trucks)
+
+
+def display_query(table: hashTable, package_id: int, report_time: datetime):
+    package: Package = table.hashSearch(package_id)
+    report_delta: timedelta = timedelta(hours=report_time.hour, minutes=report_time.minute)
+    package_info: str = f"[Package ID = {package.package_id}]"
+    if package.loading_time > report_delta:
+        package_info += "\n\tStatus: At Hub"
+    elif package.delivery_time > report_delta:
+        del_time: datetime = datetime.strptime(str(package.delivery_time), "%H:%M:%S")
+        package_info += "\n\t Status: En Route, ETA at " + del_time.strftime("%I:%M %p")
+    else:
+        del_time: datetime = datetime.strptime(str(package.delivery_time), "%H:%M:%S")
+        package_info += "\n\t Status: Delivery time at " + del_time.strftime("%I:%M %p")
+    package_info += "\n\tCity: " + package.city
+    package_info += "\n\tAddress: " + package.delivery_address
+    package_info += "\n\tZIP Code: " + package.zip_code
+    package_info += "\n\tPackage Weight: " + package.package_weight + " kilograms"
+    package_info += "\n\tDelivery Deadline: " + package.deadline
+    print(package_info)
+
+
+def prompt_time():
+    report_time: timedelta = None
+    while report_time is None:
+        try:
+            report_time = datetime.strptime(
+                input("Enter a time for the report (format [HOUR:MINUTE AM/PM]): "), "%I:%M %p")
+        except:
+            print("\tError: Invalid entry. Try again.\n")
+
+    return report_time
+
+
+def prompt_package_id(ht):
+    package_id = None
+    while package_id is None:
+        user_input = input("Please enter the ID of the package you would like to view: ")
+
+        if user_input.isdigit():
+            if ht.lookup(int(user_input)) is not None:
+                package_id = int(user_input)
+            else:
+                print("\tNo package found with the provided ID.\n")
+        else:
+            print("\tError: Invalid input. Please try again.\n")
+    return package_id
